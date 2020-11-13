880001a0beda4b71adc94e235eb1953e92ada34e
wade3po <379159075@qq.com>
2020-11-13 16:18:01 +0800

    去除测试

--- src/views/index/Index.vue
+++ src/views/index/Index.vue
@@ -1,20 +1,11 @@
 <template>
   <div class="">
-
   </div>
 </template>
 <script>
-  import {test} from "../../api/common/common";
-
+  import BenzAMRRecorder from 'benz-amr-recorder'
   export default {
-    name: '',
-    components: {},
-    data() {
-      return {}
-    },
-    mounted(){
-      // test()
-    }
+    
   }
 </script>
 <style lang="scss" type="text/scss" scoped>
